7a96e0a9d14baebc8852f71656421781ed5e6907
samrisamrawit30@gmail.com <samrisamrawit30@gmail.com>
2026-04-22 14:11:45 -0700

    Add navigation and hero section structure to Home component

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -5,6 +5,26 @@ function Home() {
 
   return (
     <>
+    <section className="hero">
+        <header className="header">
+            <nav>
+                <div className="Logo">
+                    <img src="" alt="MezmurFlow-Logo"/>
+                </div>
+                <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Features</li>
+                </ul>
+            </nav>
+        </header>
+    </section>
+    <section className="hero-content">
+        <div className="hero-card">
+            
+        </div>
+    </section>
     <h1>welcome to MezmurFlow</h1>
     <p>Find your favorite Mezmur dedicated for each day of the week</p>
     </>
