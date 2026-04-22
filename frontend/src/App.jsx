6c7ed40795bf8ed4ac8e713ee8b76223fa64b51f
samrisamrawit30@gmail.com <samrisamrawit30@gmail.com>
2026-04-22 14:05:31 -0700

    Initial frontend scaffold with React and Home component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from "./Components/Home.jsx"
+
+function App() {
+ 
+
+  return (
+    <>
+    <Home />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Home() {
+ 
+
+  return (
+    <>
+    <h1>welcome to MezmurFlow</h1>
+    <p>Find your favorite Mezmur dedicated for each day of the week</p>
+    </>
+  )
+}
+
+export default Home
