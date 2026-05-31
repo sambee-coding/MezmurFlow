461ce8f115ab8a8ea29ffaab0578288aedc5db87
samrisamrawit30@gmail.com <samrisamrawit30@gmail.com>
2026-05-31 23:14:28 +0300

    Update Origin page text and styles

--- frontend/src/Components/Origin.jsx
+++ frontend/src/Components/Origin.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import "../Components/Origin.css"
 
 function Origin(){
     return(
         <>
         <section className="Origin-Container">
             <div className="origin-content">
-                <h1>Origin/About Mezmurflow</h1>
-                <p>MezmurFlow is a web application that provides acess to daily spiritual content.
-                    A platform where users can find daily Sinkessar stories,Recommended Mezmur of the day.
+                <h1><span>Origin/About</span> <br /> <i>Mezmurflow</i></h1>
+                <p>Ever spent more time searching for the right Mezmur than actually listening to it? <br /> MezmurFlow prepares daily Mezmurs based on the spiritual meaning of each day, helping you focus on worship instead of searching
                     </p>
             
             </div>
